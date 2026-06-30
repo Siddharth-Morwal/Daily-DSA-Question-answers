48cf079792a40a81734e5289fbcb8e65d2cad7be
Siddharth-Morwal <sidmorwal3605@gmail.com>
2026-06-30 14:23:01 +0530

    Time: 82 ms (18.52%), Space: 294.3 MB (11.73%) - LeetHub

--- 3659-partition-array-into-k-distinct-groups/3659-partition-array-into-k-distinct-groups.cpp
+++ 3659-partition-array-into-k-distinct-groups/3659-partition-array-into-k-distinct-groups.cpp
@@ -3,12 +3,12 @@ public:
     bool partitionArray(vector<int>& nums, int k) {
       int n = nums.size();
       unordered_map<int , int> freq;
-     for(int f : nums) freq[f]++;
-      if(n % k) return false;
-     
-     for(auto& p : freq){
-        if(p.second > n / k) return false;
+      int grp = n / k;
+     for(int f : nums){
+        freq[f]++;
+       if(freq[f] > grp) return false;
      }
+      if(n % k) return false;
      return true;
     }
 };
