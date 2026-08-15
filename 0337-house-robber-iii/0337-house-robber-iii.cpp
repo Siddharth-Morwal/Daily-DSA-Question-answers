8150ed348e12475256ceea8863f9da0a8829ee27
Siddharth-Morwal <sidmorwal3605@gmail.com>
2026-08-15 19:33:24 +0530

    Time: 0 ms (100%), Space: 22.3 MB (99.25%) - LeetHub

---
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    pair<int , int> dfs(TreeNode* root){
+        if(!root) return{0 , 0};
+
+        auto l = dfs(root->left);
+        auto r = dfs(root->right);
+
+        int rob = root->val + l.second + r.second;
+        int skip = max(l.first , l.second) + max(r.first , r.second);
+
+        return {rob , skip};
+    }
+    int rob(TreeNode* root) {
+        auto p = dfs(root);
+        return max(p.first, p.second);
+    }
+};
